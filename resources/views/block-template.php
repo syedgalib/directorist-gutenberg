fd6c86616bb3565badeb0fbf265ba252d6d81411
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2025-11-30 15:28:14 +0600

    updated

--- resources/views/block-template.php
+++ resources/views/block-template.php
@@ -1,10 +1,21 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+use Directorist\Helper;
+
 $template = get_post( $template_id );
+
+$is_featured = Helper::is_featured( get_the_ID() );
+$class       = 'directorist-gutenberg-blocks-wrap';
+
+if ( strval( $is_featured ) === '1' ) {
+	$class .= ' directorist-gutenberg-listing-is-featured';
+}
 
 wp_enqueue_script_module( 'directorist-gutenberg/blocks-frontend' );
 ?>
 
-<div class="directorist-gutenberg-blocks-wrap">
+<div class="<?php echo esc_attr( $class ); ?>">
 	<?php echo do_blocks( $template->post_content ); ?>
 </div>
